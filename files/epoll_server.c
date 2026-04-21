14c3b09917961fb2e6824a42be9c8ad50f9a9a93
Jonathan Rodriguez-Gomez <j.rodriguezgomez@wustl.edu>
2026-04-20 23:43:59 -0500

    done.

--- files/epoll_server.c
+++ files/epoll_server.c
@@ -1,17 +1,18 @@
-#define __GNU_SOURCE
+#define _GNU_SOURCE
 #include "inet_header.h"
 #include <err.h>
 #include <fcntl.h>
 #include <limits.h>
-#include <poll.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/epoll.h>
 #include <sys/socket.h>
 #include <sys/time.h>
 #include <unistd.h>
 
 #define LISTEN_BACKLOG 50
+#define MAX_FDCOUNT 3
 
 void set_non_blocking_io(int);
 int init_socket(void);
@@ -23,96 +24,94 @@ enum fd_idx {
 };
 
 int main(void) {
-  int sfd, cfd;
+  int sfd, cfd, epoll_fd, ret, ready;
   socklen_t peer_addr_size;
   struct sockaddr_in peer_addr;
-  FILE *c_fp, *in_fp;
+  struct epoll_event ev;
+  struct epoll_event events[MAX_FDCOUNT];
   char buf[PIPE_BUF];
-  struct pollfd pollFd[3] = {0};
   int fd_cnt = 0;
   int client_cnt = 0;
 
-  // initialize STDIN pollfd
-  pollFd[in_idx].fd = STDIN_FILENO;
-  pollFd[in_idx].events = POLLIN;
-  fd_cnt += 1;
+  // initialize epoll fd
+  epoll_fd = epoll_create1(0);
+  if (-1 == epoll_fd) {
+    err(EXIT_FAILURE, "epoll_create1");
+  }
 
-  // open stdin file stream in advance
-  in_fp = fdopen(STDIN_FILENO, "r");
+  // initialize STDIN epoll_event
+  ev.events = EPOLLIN;
+  ev.data.fd = STDIN_FILENO;
+  ret = epoll_ctl(epoll_fd, EPOLL_CTL_ADD, STDIN_FILENO, &ev);
+  if (-1 == ret) {
+    err(EXIT_FAILURE, "epoll_ctl");
+  }
+  fd_cnt++;
 
   // set non blocking readers
   set_non_blocking_io(STDIN_FILENO);
 
   sfd = init_socket();
 
-  // initialize socket pollfd
-  pollFd[sfd_idx].fd = sfd;
-  pollFd[sfd_idx].events = POLLIN;
-  fd_cnt += 1;
+  // initialize socket epoll_event
+  ev.events = EPOLLIN | EPOLLET;
+  ev.data.fd = sfd;
+  ret = epoll_ctl(epoll_fd, EPOLL_CTL_ADD, sfd, &ev);
+  if (-1 == ret) {
+    err(EXIT_FAILURE, "epoll_ctl");
+  }
+  fd_cnt++;
 
   // set non blocking readers
   set_non_blocking_io(sfd);
 
-  while (poll(pollFd, fd_cnt, -1) != -1) {
-    // check for stdin input
-    if (pollFd[in_idx].revents & POLLIN) {
-      while (NULL != fgets(buf, PIPE_BUF, in_fp)) {
-        printf("Input received from STDIN: %s", buf);
-        if (!strcmp(QUIT_CMD, buf)) {
-          printf("Quit command received\n");
-          exit(EXIT_SUCCESS);
+  while ((ready = epoll_wait(epoll_fd, events, fd_cnt, -1)) != -1) {
+    for (int i = 0; i < ready; i++) {
+      if (events[i].data.fd == STDIN_FILENO) {
+        printf("Input available in STDIN\n");
+        /* while (read(STDIN_FILENO, buf, PIPE_BUF) > 0); */
+      } else if (events[i].data.fd == cfd) {
+        if (events[i].events & EPOLLRDHUP) {
+          /* printf("DEBUG: client POLLHUP triggered\n"); */
+          printf("Client %d has closed the connection\n", client_cnt);
+          ret = epoll_ctl(epoll_fd, EPOLL_CTL_DEL, cfd, NULL);
+          if (-1 == ret) {
+            err(EXIT_FAILURE, "epoll_ctl");
+          }
+
+          if (-1 == close(cfd))
+            err(EXIT_FAILURE, "fclose");
+          fd_cnt -= 1;
+        } else if (events[i].events & EPOLLIN) {
+          printf("Input available from Client %d\n", client_cnt);
         }
-        printf("\n");
+
+      } else if (events[i].data.fd == sfd) {
+        if (events[i].events & EPOLLIN) {
+          cfd = accept(sfd, (struct sockaddr *)&peer_addr, &peer_addr_size);
+
+          if (cfd == -1)
+            err(EXIT_FAILURE, "accept");
+
+          client_cnt++;
+
+          // initialize socket pollfd
+          ev.events = EPOLLIN | EPOLLRDHUP | EPOLLET;
+          ev.data.fd = cfd;
+          ret = epoll_ctl(epoll_fd, EPOLL_CTL_ADD, cfd, &ev);
+          if (-1 == ret) {
+            err(EXIT_FAILURE, "epoll_ctl");
+          }
+          fd_cnt++;
+
+          // set non blocking readers
+          set_non_blocking_io(sfd);
+        }
       }
     }
-
-    if (fd_cnt > 2 && pollFd[cfd_idx].revents & POLLIN) {
-      /* printf("DEBUG: client POLLIN triggered\n"); */
-      while (NULL != fgets(buf, PIPE_BUF, c_fp)) {
-        printf("Client %d: %s", client_cnt, buf);
-        if (!strcmp(QUIT_CMD, buf)) {
-          printf("Quit command received\n");
-          exit(EXIT_SUCCESS);
-        }
-      }
-    }
-
-    if (fd_cnt > 2 && pollFd[cfd_idx].revents & POLLHUP) {
-      /* printf("DEBUG: client POLLHUP triggered\n"); */
-      if (-1 == fclose(c_fp))
-        err(EXIT_FAILURE, "fclose");
-      fd_cnt -= 1;
-    }
-
-    if (pollFd[sfd_idx].revents & POLLIN) {
-      /* printf("DEBUG: socket POLLIN triggered\n"); */
-      cfd = accept(sfd, (struct sockaddr *)&peer_addr, &peer_addr_size);
-
-      if (cfd == -1)
-        err(EXIT_FAILURE, "accept");
-
-      client_cnt++;
-
-      // initialize socket pollfd
-      pollFd[cfd_idx].fd = cfd;
-      pollFd[cfd_idx].events = POLLHUP | POLLIN;
-      fd_cnt += 1;
-
-      // set non blocking readers
-      set_non_blocking_io(sfd);
-
-      // prematurely create FILE *
-      c_fp = fdopen(cfd, "r");
-
-      if (NULL == c_fp)
-        err(EXIT_FAILURE, "fdopen");
-
-      setbuf(c_fp, NULL);
-    }
-
   }
 
-  if (fclose(in_fp) == -1) {
+  if (close(STDIN_FILENO) == -1) {
     err(EXIT_FAILURE, "fclose");
   }
 
